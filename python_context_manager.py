c1c980887a7177ca09a720724dc3010c2d6c7594
Mahendra Kumar P <mahendranani515@gmail.com>
2025-10-04 17:29:58 +0530

    Add Python example: python_context_manager.py

---
+++ python_context_manager.py
@@ -0,0 +1,17 @@
+class FileManager:
+    def __init__(self, filename, mode):
+        self.filename = filename
+        self.mode = mode
+        self.file = None
+
+    def __enter__(self):
+        self.file = open(self.filename, self.mode)
+        return self.file
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        if self.file:
+            self.file.close()
+
+# Usage
+# with FileManager('test.txt', 'w') as f:
+#     f.write('Hello World')
