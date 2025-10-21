5e2eaf14f0fa10c0562e402dd5bd64db22f86827
Mahendra Kumar P <mahendranani515@gmail.com>
2025-10-21 22:48:39 +0530

    Add JavaScript example: js_closures.js

---
+++ js_closures.js
@@ -0,0 +1,21 @@
+function createCounter() {
+    let count = 0;
+    return {
+        increment: function() {
+            count++;
+            return count;
+        },
+        decrement: function() {
+            count--;
+            return count;
+        },
+        getValue: function() {
+            return count;
+        }
+    };
+}
+
+const counter = createCounter();
+console.log(counter.increment()); // 1
+console.log(counter.increment()); // 2
+console.log(counter.getValue());  // 2
